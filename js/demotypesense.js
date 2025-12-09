e563f770ae34e0f21c86392c7477c602a8fb1ead
minhngdinh <66162003+minhngdinh@users.noreply.github.com>
2025-12-09 09:38:43 +0700

    Update demotypesense.js

--- js/demotypesense.js
+++ js/demotypesense.js
@@ -39,7 +39,7 @@ autocomplete({
     }
 
     const results = await typesenseClient
-    .collections('address_demo_hcm_2')
+    .collections('address_demo_hcm_3')
     .documents()
     .search(searchParams);
 
